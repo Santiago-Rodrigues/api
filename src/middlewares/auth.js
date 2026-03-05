e36019ad91268bc0edd32d9fa62d37742c055071
Santiago Rodrigues <santiagosrod@outlook.com>
2026-03-05 08:49:26 -0300

    remove log statement from auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,8 +5,6 @@ const SECRET = process.env.JWT_SECRET
 const auth = (req, res, next) => {
   const token = req.headers.authorization
 
-  console.log(token)
-
   if (!token) {
     return res.status(401).json({ message: "Acesso negado" })
   }
